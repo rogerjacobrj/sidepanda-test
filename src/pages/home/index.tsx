3b0440d1175032bc70534ed0e44328a45bb4fc6a
Roger Jacob <rogerjacobrj@gmail.com>
2024-05-22 11:11:37 +0530

    error boundary addition

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,17 @@
 import Header from '../../components/header';
 import Scheduler from '../../components/scheduler';
 import './styles.scss';
+import ErrorBoundary from '../../components/error-boundary';
+import ErrorFallback from '../../components/error-fallback';
 
 const Home = () => {
     return (
-        <div>
-            <Header />
-            <Scheduler />
-        </div>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <div>
+                <Header />
+                <Scheduler />
+            </div>
+        </ErrorBoundary>
     )
 };
 
